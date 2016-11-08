fce6f443699c53c26bce17677a0198fc4c424dc0
rudy <misha.shchetinin@gmail.com>
2016-11-08 12:26:36 +0200

    added testcases for registration methon in service

--- frontend/src/common/user.service.spec.js
+++ frontend/src/common/user.service.spec.js
@@ -1,11 +1,13 @@
 describe('Users Service', function() {
-  var UserService;
+  var UserService,
+    $httpBackendMock;
 
   beforeEach(function() {
     module('public');
 
     inject(function($injector) {
       UserService = $injector.get('UserService');
+      $httpBackendMock = $injector.get('$httpBackend');
     });
   });
 
@@ -17,5 +19,27 @@ describe('Users Service', function() {
     it('should exist', function() {
       expect(UserService.registerUser).toBeDefined();
     });
+    it('should register user if username and email provided', function() {
+      $httpBackendMock.expectPOST('/api/users').respond({
+        message: 'User registered',
+        status: 'success'
+      });
+      UserService.registerUser().then(function(response) {
+        expect(response.status).toEqual('success');
+        expect(response.message).toEqual('User registered');
+      });
+      $httpBackendMock.flush();
+    });
+    it('should raise an error if username or email are not provided', function() {
+      $httpBackendMock.expectPOST('/api/users').respond({
+        message: 'Username and email are required',
+        status: 'error'
+      });
+      UserService.registerUser(null).then(function(response) {
+        expect(response.status).toEqual('error');
+        expect(response.message).toEqual('Username and email are required');
+      });
+      $httpBackendMock.flush();
+    })
   });
 });
