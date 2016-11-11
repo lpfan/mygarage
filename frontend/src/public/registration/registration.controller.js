b6589fbd664649500d3ea57c6a3b86ec17ae91f2
rudy <misha.shchetinin@gmail.com>
2016-11-11 16:44:46 +0200

    modify registration page

--- frontend/src/public/registration/registration.controller.js
+++ frontend/src/public/registration/registration.controller.js
@@ -4,16 +4,19 @@
   angular.module('public')
     .controller('RegistrationController', RegistrationController);
 
-  RegistrationController.$inject = ['UserService', '$location'];
+  RegistrationController.$inject = ['UserService', '$location', '$scope`'];
 
-  function RegistrationController(UserService, $location) {
+  function RegistrationController(UserService, $location, $scope) {
     var $ctrl = this;
 
     $ctrl.submit = function(user) {
       UserService.registerUser(user)
         .then(function(response) {
           if (response.status === 'success') {
+            $scope.informMessage = 'Registration successful'
             $location.path('/');
+          } else {
+            $scope.informMessage = response.message;
           }
         });
     };
--- frontend/src/public/registration/registration.spec.js
+++ frontend/src/public/registration/registration.spec.js
@@ -1,48 +1,29 @@
 describe('RegistrationController', function() {
   var $controller,
+    $scope,
+    $location,
     RegistrationController,
     UserService;
 
   beforeEach(angular.mock.module('ui.router'));
   beforeEach(angular.mock.module('public'));
 
-  beforeEach(
-    inject(function($injector) {
-      $controller = $injector.get('$controller');
-      UserService = $injector.get('UserService');
-      RegistrationController = $controller('RegistrationController', {
-        UserService: UserService
-      });
-    }));
+  beforeEach(inject(function(_$controller_, _$rootScope_, _$location_, _UserService_) {
+    $controller = _$controller_;
+    UserService = _UserService_;
+
+    $scope = _$rootScope_.$new();
+    $location = _$location_;
+
+    RegistrationController = $controller('RegistrationController', {
+      UserService: UserService,
+      $location: $location,
+      $scope: $scope
+    });
+  }));
 
   it('should be defined with UserService', function() {
     expect(RegistrationController).toBeDefined();
     expect(UserService).toBeDefined();
-  });
-});
-
-describe('Registration process', function() {
-  var $scope;
-  var $q;
-  var deferred;
-  var $location;
-  var RegistrationController;
-
-  beforeEach(module('public'));
-
-  beforeEach(inject(function($controller, _$rootScope_, _$q_, _$location_, UserService) {
-    $location = _$location_;
-    spyOn($location, 'path');
-
-    RegistrationController = $controller('RegistrationController', {
-      UserService: UserService,
-      $location: $location
-    });
-  }));
-
-  it('should call $location to redirect if success', function() {
-    expect(RegistrationController).toBeDefined();
-    RegistrationController.submit(null);
-    expect($location.path).toHaveBeenCalledWith('/');
   });
 });
